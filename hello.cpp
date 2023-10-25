649fbf8171cfdda7ea3ea156fc86066112a59f2c
Solomon <kyobenimaru001@gmail.com>
2023-10-25 15:59:37 +0300

    commit for the welcome message :)

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  string name;
+
+  cout<<"Hello World!\n";
+
+  cin>>name;
+
+  cout<<"Hello "<<name<< " and welcome to the circus"<<endl;
+
+  return (0);
+}
